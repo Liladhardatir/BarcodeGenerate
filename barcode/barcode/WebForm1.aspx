0d87e4a4e33d261153dff5811c3006108e1d2ca1
liladhar <liladhardatir16@gmail.com>
2023-06-29 22:16:46 +0530

    barcode installing zxing using asp .net web application empty

---
+++ barcode/barcode/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="barcode.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Generate and Read QR code in Asp.Net</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtCode" runat="server"></asp:TextBox>
+            <asp:Button ID="BtnGenerate" runat="server" Text="Generate QR Code" OnClick="btnGenerate_click" />
+            <hr />
+            <asp:Image ID="imgQRCode" Width="100px" Height="100px" runat="server" Visible="false" />
+            <br /><br />
+             <asp:Button ID="btnRead" runat="server" Text="Read QR Image" OnClick="btnRead_click" />
+            <br /><br />
+            <asp:Label ID="lbQRCode" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ barcode/barcode/WebForm1.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using ZXing;
+using System.CodeDom.Compiler;
+
+
+namespace barcode
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGenerate_click(object sender, EventArgs e)
+        {
+
+            {
+                {
+                    GenerateCode(txtCode.Text);
+                }
+            }
+
+        }
+
+        protected void btnRead_click(object sender, EventArgs e)
+        {
+            ReadQRCode();
+        }
+        //Generate QRCode 
+        private void GenerateCode(string name)
+        {
+            var writer=new BarcodeWriter();
+            writer.Format=BarcodeFormat.QR_CODE;
+            var result=writer.Write(name);
+            string path = Server.MapPath("~/image/QRImage.jpg");
+            var barcodeBitmap=new Bitmap(result);
+
+            using (MemoryStream memory=new MemoryStream())
+            {
+                using(FileStream Fs=new FileStream(path, FileMode.Create,FileAccess.ReadWrite))
+                {
+                    barcodeBitmap.Save(memory, ImageFormat.Jpeg);
+                    byte[] bytes=memory.ToArray();
+                    Fs.Write(bytes,0,bytes.Length);
+
+                }
+            }
+            imgQRCode.Visible=true;
+            imgQRCode.ImageUrl = "~/image/QRImage.jpg";
+
+        }
+        //Read code from QR Image
+        private void ReadQRCode()
+        {
+            var reader=new BarcodeReader();
+            string filename = Path.Combine(Request.MapPath("~/images"), "QRImage.jpg");
+            //Detach and decode the barcode inside the bitmap
+            var result = reader.Decode(new Bitmap(filename));
+            if (result != null)
+            {
+                lbQRCode.Text = "QR Code:  "+result.Text;
+            }
+        }
+    }
+}
